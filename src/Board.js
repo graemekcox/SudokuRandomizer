8bab2d143c8f16924160a4f99ebce87a451ded2b
graemekcox <graeme.k.cox@gmail.com>
2020-07-06 18:16:20 -0400

    Check for valid values before updating the board

--- src/Board.js
+++ src/Board.js
@@ -8,69 +8,122 @@ class Square extends React.Component{
         super(props);
         this.state = {
             id: this.props.id
+            // squares
             // id: 1
             // key: 0,
-            // squares: Array(81).fill(null),
+            // squares: this.props.squares
             // value: "" // TODO need to make sure only numbers are input
             // value: 0
         }
         // this.handleChange = this.handleChange.bind(this); // TODO is this necessary?
     }
 
-    sumRows() {
-        var sum, row, col;
+    check_row(new_val, row)
+    {
+        var values = Array(9);
 
-        // Get current col and row
-        col = this.props.key % 9;
-        row = Math.floor(Number(this.props.key / 9));
-
-        sum = 0;
-        // go through all row indexes, and make sure current value is unique
         for (var ind= row*9; ind<( (row+1) * 9); ind ++){
-            // sum+= this.props.squares[ind].value;
+            if (this.props.squares[ind] != null)
+                values[ind] = Number(this.props.squares[ind]);
+            else   
+                values[ind] = 0;
+        }
+        if (values.indexOf(new_val) != -1){ // Check if this current value is within the array already
+            return -1;
         }
 
-        // console.log("SQUARES = %0d" + this.props.squares[0])
-        console.log("VALUE = " + this.props.value)
-        console.log("COL "+ col + " : ROW = " + row)
+    }
 
-        return sum;
+    check_col(new_val, col){
+        var values = Array(9);
+
+        for (var ind=col; ind< (9*9)   ; ind=ind+9){
+            if (this.props.squares[ind] != null)
+                values[ind] = Number(this.props.squares[ind]);
+            else   
+                values[ind] = 0;
+        }
+        if (values.indexOf(new_val) != -1){ // Check if this current value is within the array already
+            return -1;
+        }
+    }
+
+    check_box(new_val, row, col){
+        var values = Array(9);
+
+        var start_col = Math.floor(col / 3);
+        var start_row = Math.floor(row / 3) * 3;
+        var start_ind = start_row * 9 + start_col;
+
+        for (var i=0; i < 3 ; i++) {
+            for (var j=0; j<3; j++){
+                var ind = start_ind + j + (i*9);
+                if (this.props.squares[ind] != null)
+                    values[ind] = Number(this.props.squares[ind]);
+                else
+                    values[ind] = 0;
+            }
+        }
+        if (values.indexOf(new_val) != -1)
+            return -1;
+    }
+
+    check_if_valid(new_val) {
+        var sum, row, col;
+        // var values = Array(9);
+        console.log("ID = " + this.state.id + ": New val = " + new_val)
+
+        // console.log(this.props.squares)
+
+        // Get current col and row
+        col = (this.state.id) % 9;
+        row = Math.floor(Number(this.state.id / 9));
+
+        console.log("COL "+ col + " : ROW = " + row)
+        sum = 0;
+        // go through all row indexes, and make sure current value is unique
+        
+        if (this.check_row(new_val, row) == -1)
+            return -1;
+
+        if (this.check_col(new_val, col) == -1)
+            return -1;
+
+        if (this.check_box(new_val, row, col) == -1)
+            return -1;
+
+        // If all this is unique for all sections, then value can be updated
+        return new_val;
     }
 
     onFieldChange(event) {
-        // if ((event.target.value < 1 || event.target.value > 9) && event.target.value != "") {
-        //     // TODO Contrain values to be within this range
-        //     // TODO, make square go red for a bit when value is bad?
-        //     console.log("THIS VALUE IS BAD " + event.target.value + ":   " +this.props.id)
-        //     this.setState({value: "" })
+        var new_val;
+        if ((event.target.value < 1 || event.target.value > 9) && event.target.value != "") {
+            // TODO Contrain values to be within this range
+            // TODO, make square go red for a bit when value is bad?
+            this.setState({value: "" })
+            new_val = 0;
+        }
+        else { // Only update state if it within 1-9
+            // TODO only update state if this is a valid number
 
-        // }
-        // else { // Only update state if it within 1-9
-        //     // TODO only update state if this is a valid number
-        //     console.log("THE CURRENT VALUE IS = " + event.target.value)
-            
-        //     // this.sumRows();
+            new_val = event.target.value;
+            if (this.check_if_valid(Number(new_val)) == -1)
+                new_val = 0;
+        }
 
-        //     this.setState({ value: event.target.value});
-        //     // this.setState({ squares[this.prop]: event.target.value});
-
-        //     // const fieldName = event.target.name;
-        //     // const fieldValue = event.target.value;
-        //     // this.props.onChange(fieldName, fieldValue);
-        //     console.log(this.props)
-        // }
         const fieldName = event.target.name;
-        const fieldValue = event.target.value;
-
-
-        this.props.onChange(this.state.id, fieldName, fieldValue)
+        
+        this.props.onChange(this.state.id, fieldName, new_val)
+        // this.props.onChange(this.state.id, fieldName, fieldValue)
     }
 
     render() {
         return (
             <input 
                 className="square" 
-                value={this.props.value}
+                value={this.props.squares[this.props.id]}
+                // value={this.props.value}
                 onChange={this.onFieldChange.bind(this)}
             >
                  {/* {this.props.value} */}
@@ -88,42 +141,26 @@ class Board extends React.Component{
     }
 
     onChange(i, field, value) {
+        
         const squares = this.state.squares.slice();
+        
+        // value = this.sumRows(i, Number(value))
+        // if (value != -1) // If -1, then this value is not valid
         squares[i]= value;
+        
         this.setState({squares: squares});
     }
 
     renderSquare(i) {
         return <Square
             id={i}
+            squares={this.state.squares}
             onChange={this.onChange.bind(this)}
             value={this.state.squares[i]}
         >
         
         </Square>;
     }
-
-    // sumRows() {
-    //     var sum, row, col;
-
-    //     // Get current col and row
-    //     col = this.props.id % 9;
-    //     row = Math.floor(Number(this.props.id / 9));
-
-    //     sum = 0;
-    //     // go through all row indexes, and make sure current value is unique
-    //     for (var ind= row*9; ind<( (row+1) * 9); ind ++){
-    //         sum+= this.props.squares[ind].value;
-    //     }
-
-    //     console.log("COL "+ col + " : ROW = " + row)
-
-    //     return sum;
-    // }
-    // componentDidUpdate(prevProps) {
-    //     console.log("UPDATED")
-    //     // TODO Sum all rows and colms to make sure 
-    // }
 
     render(){        
         var squares = []
